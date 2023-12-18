e3bd66fa4cf7a9d76bd5401f50dc30d101f6bbfe
Elizaveta Shabalkina <shabalkina.liza1999@icloud.com>
2023-12-18 23:56:45 +0300

    test commit

---
+++ src/ru/mirea/task2/task2_1/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.task2.task2_1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author test = new Author("Test", "test@test.org", 'n');
+
+        System.out.println(test);
+    }
+}
